90c9997f0c08996c6f2b89a17e46d4c7cb718734
Dhanush Reddy <dhanush.v43431@gmail.com>
2025-06-20 21:39:55 +0100

    functions script

---
+++ functions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+user=$(id -u)
+
+validate() {
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 failure"
+        exit 1
+    else
+        echo "$2 Success"
+    fi
+}
+
+if [ user -ne 0 ]
+then
+    echo "you must have sudo access"
+    exit 1
+fi
+
+dnf list installed mysql-community-server
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql-community-server -y
+    validate $? "Installing MySql"
+else
+    echo "My Sql already installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    validate $? "Installing Git"
+else
+    echo "Git is already installed"
+fi
